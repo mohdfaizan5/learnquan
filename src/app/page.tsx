14e1fba27a6bd030ba8dbeab60a25c0dcc0deb36
Mohammed Faizan <mohdfaizan.zprb2@gmail.com>
2024-06-30 21:45:36 +0530

    add congrats page also

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import { Button } from '@/components/ui/button'
+import { Star } from 'iconsax-react'
+import Image from 'next/image'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      <Button variant="outline">Button</Button>
+    <div className='flex flex-col items-center justify-center min-h-screen'>
+      <Image className='animate-pulse' src={"/svgs/cute-smiling-cat.svg"} height={200} width={200} alt='happy'/>
+      <h2 className='text-center'>Lesson <br/> Complete!</h2>
+      <p className='font-light text-xs'>Total XP</p>
+      <h1 className='text-center flex items-center gap-2'>40<Star className='text-green-700'/></h1>
+      <Button className='w-52 mt-20'>Continue</Button>
 
     </div>
   )
--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -12,32 +12,72 @@ const checkQuizAnswer = (userInput: string, correctAnswer: string): boolean => {
 
 const page = () => {
   const { register, handleSubmit } = useForm();
+  const question = {
+    question: "What is the capital of France?",
+    options: ["London", "Paris", "Berlin"],
+    answer: 1,
+  };
+  const handleQuiz = (data: any) => {
+    console.log(data); // Now data.answer should hold "correctOption" or "incorrectOption"
+    // console.log(question.options[question.answer] == data.answer)
+    if(question.options[question.answer] == data.answer){
+      
+    }
+  };
+
   return (
     <div>
       What does the program display if you remove the quotes around the message?
       (Edit the program above to find out.)
-      <form
-        onSubmit={handleSubmit((data) => {
-          console.log(data);
-          // check whether answer is true
-        })}
-      >
-        <input {...register("a", { value: "1" })} />
-        {/* How do we take which button was selected */}
-        <input {...register("b")} />
-        {/* TODO: How do we take input on radio input */}
+      <form onSubmit={handleSubmit((data) => handleQuiz(data))}>
+        {question.question}
+        {question.options.map((option, index) => (
+          <Label
+            className="flex items-center w-full mb-1 px-5 py-3 gap-2 ps-4 border border-gray-200 rounded "
+            htmlFor={`${option}${index}`}
+          >
+            <input
+              id={`${option}${index}`}
+              {...register("answer")}
+              value={option}
+              type="radio"
+            />
+            {option}
+          </Label>
+        ))}
 
-        {/* <Label
-          className="flex items-center w-full mb-1 px-5 py-3 gap-2 ps-4 border border-gray-200 rounded "
-          htmlFor={"1"}
-        >
-          <input id={"1"} {...register("a", { value: "1" })} type="radio" />
-          Hi
-        </Label> */}
         <Button>Submit</Button>
       </form>
     </div>
   );
 };
-
 export default page;
+{
+  /* How do we take which button was selected */
+}
+{
+  /* TODO: How do we take input on radio input */
+}
+{
+  /* <Label
+          className="flex items-center w-full mb-1 px-5 py-3 gap-2 ps-4 border border-gray-200 rounded "
+          htmlFor={"1"}
+        >
+          <input id={"1"} {...register("a", { value: "1" })} type="radio" />
+          Hi
+        </Label> 
+        
+          const questions = [
+    {
+      question: "What is the capital of France?",
+      options: ["London", "Paris", "Berlin"],
+      answer: "B",
+    },
+    {
+      question: "What is the largest planet in our solar system?",
+      options: ["Earth", "Jupiter", "Mars"],
+      answer: "B",
+    },
+  ];
+        */
+}

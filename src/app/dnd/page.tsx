5eb0fcf98f0a72c4f5519f555a153ed7d129d301
Mohammed Faizan <mohdfaizan.zprb2@gmail.com>
2024-07-02 20:26:54 +0530

    framer motion reorder

--- src/app/dnd/page.tsx
+++ src/app/dnd/page.tsx
@@ -6,32 +6,50 @@ import { Reorder } from "framer-motion";
 
 const page = () => {
   const [items, setItems] = useState([1, 2, 3, 4, 5]);
+  const [items2, setItems2] = useState(["A", "B", "C", "D", "E"]);
 
   return (
-    <div>
-      <Card className="w-96">
-        <CardHeader>
-          <CardTitle>Card Title</CardTitle>
-        </CardHeader>
-        <CardContent className="flex flex-col">
-          <Reorder.Group
-            values={items}
-            onReorder={(e) => {
-              console.log(e);
-              setItems(e);
-            }}
-          >
-            {items.map((item, i) => (
-              <Reorder.Item value={item} key={i}>
-                <p className="flex w-full justify-between p-1 border rounded-sm  items-center px-4 bg-slate-50">
-                  Card {item}
-                  <CgMenuGridO />
-                </p>
-              </Reorder.Item>
-            ))}
-          </Reorder.Group>
-        </CardContent>
-      </Card>
+    // <div>
+    //   <Card className="w-96">
+    //     <CardHeader>
+    //       <CardTitle>Card Title</CardTitle>
+    //     </CardHeader>
+    //     <CardContent className="flex flex-col">
+    //       <Reorder.Group
+    //         values={items}
+    //         onReorder={(e) => {
+    //           console.log(e);
+    //           setItems(e);
+    //         }}
+    //       >
+    //         {items.map((item, i) => (
+    //           <Reorder.Item value={item} key={i}>
+    //             <p className="flex w-full justify-between p-1 border rounded-sm  items-center px-4 bg-slate-50">
+    //               Card {item}
+    //               <CgMenuGridO />
+    //             </p>
+    //           </Reorder.Item>
+    //         ))}
+    //       </Reorder.Group>
+    //     </CardContent>
+    //   </Card>
+    // </div>
+    <div className="flex gap-20 justify-center">
+      <Reorder.Group axis="y" values={items} onReorder={setItems}>
+        {items.map((item) => (
+          <Reorder.Item key={item} value={item}>
+            {item}
+          </Reorder.Item>
+        ))}
+      </Reorder.Group>
+      <hr />
+      <Reorder.Group axis="y" values={items2} onReorder={setItems2}>
+        {items2.map((item) => (
+          <Reorder.Item key={item} value={item}>
+            {item}
+          </Reorder.Item>
+        ))}
+      </Reorder.Group>
     </div>
   );
 };
